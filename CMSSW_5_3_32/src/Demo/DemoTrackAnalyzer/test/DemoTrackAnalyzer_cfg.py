7f698bd22ba1b37d4cccd550c00db5816070f634
Felipe <felinavarro94@gmail.com>
2017-10-05 18:58:10 -0500

    new configuration file

--- CMSSW_5_3_32/src/Demo/DemoTrackAnalyzer/test/DemoTrackAnalyzer_cfg.py
+++ CMSSW_5_3_32/src/Demo/DemoTrackAnalyzer/test/DemoTrackAnalyzer_cfg.py
@@ -2,8 +2,13 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Demo")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100))
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(#'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/MuHad/AOD/12Oct2013-v1/00000/006A70B9-7845-E311-ADC1-003048679188.root'
- 'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Photon/AOD/12Oct2013-v1/10000/AC980FCC-943D-E311-A1D6-003048F11824.root'   ))
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+  #'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/MuHad/AOD/12Oct2013-v1/00000/006A70B9-7845-E311-ADC1-003048679188.root'
+
+ # 'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/Photon/AOD/12Oct2013-v1/10000/AC980FCC-943D-E311-A1D6-003048F11824.root'  
+  'file:reco_test.root'
+
+ ))
 process.demo = cms.EDAnalyzer("DemoTrackAnalyzer", tracks = cms.untracked.InputTag("generalTracks"))
 process.p = cms.Path(process.demo)
 
